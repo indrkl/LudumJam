eeec0bb8540618258a4d0cffd0ca50d35622e85e
hendrikeerikson <hendrik.eerikson@gmail.com>
2016-04-16 14:57:24 +0300

    Player Object

---
+++ Scripts/ControllerPlayer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerPlayer : MonoBehaviour {
+    Rigidbody2D body;
+    EntityBase manager;
+    PlayerForm form;
+    float speed;
+    float jumpHeight;
+
+    //state that the player spawns in
+    public int startingForm;
+
+	void Start () {
+        body = gameObject.GetComponent<Rigidbody2D>();
+        manager = gameObject.GetComponent<EntityBase>();
+        form = gameObject.GetComponent<PlayerForm>();
+
+        //get player maximum speed and jump height
+        speed = manager.speed;
+        jumpHeight = manager.jumpHeight;
+
+
+	}
+	
+	void Update () {
+        float movement = Input.GetAxis("Horizontal") * speed;
+        movement *= Time.deltaTime;
+        body.velocity = new Vector2(movement, body.velocity.y);
+
+        float jump = Input.GetAxisRaw("Jump") * jumpHeight;
+
+        if (Mathf.Abs(jump) > 0.1) {
+            body.velocity = new Vector2(body.velocity.x, jump);
+        }
+
+	}
+}
---
+++ Scripts/EntityBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityBase : MonoBehaviour {
+    public float speed;
+    public float jumpHeight;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
