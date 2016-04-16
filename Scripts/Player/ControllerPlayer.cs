f6af632f4cc8e5fb82ea771f7721403cfb6c703c
hendrikeerikson <hendrik.eerikson@gmail.com>
2016-04-16 19:48:53 +0300

    animation töötab enamvähem

--- Scripts/Player/ControllerPlayer.cs
+++ Scripts/Player/ControllerPlayer.cs
@@ -31,19 +31,22 @@ public class ControllerPlayer : EntityBase {
 
         float jump = Input.GetAxisRaw("Jump") * jumpHeight;
 
+        //test if jump is possible
         if (Mathf.Abs(jump) > 0.1)
         {
-            RaycastHit2D testJump = Physics2D.Raycast(transform.position, Vector2.down, 10);
+            RaycastHit2D testJump = Physics2D.Raycast(transform.position + new Vector3(0, 0.5f, 0), Vector2.down, 10);
             if (testJump.collider != null)
             {
                 float dist = Mathf.Abs(testJump.point.y - transform.position.y);
                 print(dist);
-                if (dist < 1)
+                if (dist < 0.6f)
                 {
                         body.velocity = new Vector2(body.velocity.x, jump);
                 }
             }
         }
+
+        //flip direction
         if (movement > 0)
         {
             direction = "RIGHT";
@@ -54,5 +57,15 @@ public class ControllerPlayer : EntityBase {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
 
-	}
+        AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
+
+        if (Mathf.Abs(movement) < 0.05 && !currentState.IsName("CatRun")) {
+            anim.SetBool("IsIdle", true);
+        } else {
+            anim.SetBool("IsIdle", false);
+        }
+
+
+
+    }
 }
