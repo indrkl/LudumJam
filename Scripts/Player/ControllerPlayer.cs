f82b91519c3a3e0afccfcad0e079f6d29464484b
hendrikeerikson <hendrik.eerikson@gmail.com>
2016-04-16 22:24:06 +0300

    Cat attack

--- Scripts/Player/ControllerPlayer.cs
+++ Scripts/Player/ControllerPlayer.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class ControllerPlayer : EntityBase {
     public PlayerForm form;
-
+    public float cooldown;
+    float cd_remaining;
     //state that the player spawns in
 
 
@@ -11,11 +12,13 @@ public class ControllerPlayer : EntityBase {
     void Start()
     {
         form = gameObject.GetComponent<PlayerForm>();
+        cd_remaining = 0;
 
         //get player maximum speed and jump height
     }
 
     void Update () {        
+        //switch form
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             form.SwitchForm(1);
@@ -23,7 +26,23 @@ public class ControllerPlayer : EntityBase {
         {
             form.SwitchForm(0);
         }
-        
+
+        //attack
+        if (Input.GetKeyDown(KeyCode.Q) && cd_remaining == 0)
+        {
+            anim.SetTrigger("Attack");
+            cd_remaining = cooldown;
+        }
+
+        if (cd_remaining > 0)
+        {
+            cd_remaining -= Time.deltaTime;
+        }
+        if (cd_remaining < 0)
+        {
+            cd_remaining = 0;
+        }
+
         AnimatorStateInfo currentState = anim.GetCurrentAnimatorStateInfo(0);
 
         if (Mathf.Abs(movement) < 0.05 && currentState.IsName("CatIdle"))
