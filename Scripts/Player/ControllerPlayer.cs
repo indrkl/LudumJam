f7ee08db524efcb297f04ba0ff26c131210fffeb
hendrikeerikson <hendrik.eerikson@gmail.com>
2016-04-16 18:59:38 +0300

    Cat jump

--- Scripts/Player/ControllerPlayer.cs
+++ Scripts/Player/ControllerPlayer.cs
@@ -19,25 +19,31 @@ public class ControllerPlayer : EntityBase {
 
     void Update () {
         float movement = Input.GetAxis("Horizontal") * speed;
-        movement *= Time.deltaTime;
         body.velocity = new Vector2(movement, body.velocity.y + 0.1f);
 
         //controll movment animation
-
-        print(movement);
-        if (Mathf.Abs(movement) > 0.1) {
+        if (Mathf.Abs(movement) > 0.01) {
             anim.SetFloat("Speed", 1);
-            anim.Play("catRun", 0);
+            //anim.Play("catRun", 0);
         } else {
             anim.SetFloat("Speed", 0);
         }
 
         float jump = Input.GetAxisRaw("Jump") * jumpHeight;
 
-        if (Mathf.Abs(jump) > 0.1) {
-            body.velocity = new Vector2(body.velocity.x, jump);
+        if (Mathf.Abs(jump) > 0.1)
+        {
+            RaycastHit2D testJump = Physics2D.Raycast(transform.position, Vector2.down, 10);
+            if (testJump.collider != null)
+            {
+                float dist = Mathf.Abs(testJump.point.y - transform.position.y);
+                print(dist);
+                if (dist < 1)
+                {
+                        body.velocity = new Vector2(body.velocity.x, jump);
+                }
+            }
         }
-
         if (movement > 0)
         {
             direction = "RIGHT";
