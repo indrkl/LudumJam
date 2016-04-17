9ef8e8bbe1fc42a81eac7be2c71778109318648b
indrkl <indrkl@gmail.com>
2016-04-17 11:35:35 +0300

    WIP

---
+++ Scripts/GameLogic/Level.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level : MonoBehaviour {
+    public List<GameObject> enemyPrefabs;
+
+
+    public GameObject getSpawn()
+    {
+        return enemyPrefabs[Random.Range(0, enemyPrefabs.Count)];
+
+    }
+}
