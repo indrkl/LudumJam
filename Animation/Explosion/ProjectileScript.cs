dbeedb725ad17add9d19035c9f03d71bc1f4969c
hendrikeerikson <hendrik.eerikson@gmail.com>
2016-04-18 12:33:53 +0300

    Projectile stuff and rock texture

--- Animation/Explosion/ProjectileScript.cs
+++ Animation/Explosion/ProjectileScript.cs
@@ -11,25 +11,11 @@ public class ProjectileScript : MonoBehaviour {
         if (circle.IsTouchingLayers(8))
         {
             RaycastHit2D hit = Physics2D.Raycast(gameObject.transform.position, body.velocity.normalized);
-            gameObject.GetComponent<EntityBase>().takeDamage(30f, 1);
+            gameObject.GetComponent<EntityBase>().takeDamage(45f, 1);
             Destroy(gameObject);
 
         }
         else if (circle.IsTouchingLayers(9))
-        {
-            Destroy(gameObject);
-        }
-    }
-
-	void OnCollisionEnter2D(Collision2D col)
-    {
-        print(col.gameObject);
-        if (col.transform.gameObject.layer == 8)
-        {
-            col.transform.gameObject.GetComponent<EntityBase>().takeDamage(50f, 1);
-            Destroy (gameObject);
-
-        } else if (col.transform.gameObject.layer == 9)
         {
             Destroy(gameObject);
         }
