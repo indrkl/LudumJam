661b62c24255c0a247050f4e18e6bed8dff94479
Joonatan Samuel <joonatan.samuel@gmail.com>
2016-04-18 12:36:55 +0300

    DeadEntity folder

---
+++ Scripts/DeadEntity/DeadBody.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeadBody : MonoBehaviour {
+	public bool isCollectable;
+	public int formID;
+
+	void OnTriggerEnter2D(Collider2D obj) {
+		if (obj.name == "Player")
+			isCollectable = true;
+			//Debug.Log ("I can feel the vibes of the player");
+	}
+
+	void OnTriggerExit2D(Collider2D obj){
+		if (obj.name == "Player")
+			isCollectable = false;
+			//Debug.Log ("Player vibes left me"); 
+	}
+}
